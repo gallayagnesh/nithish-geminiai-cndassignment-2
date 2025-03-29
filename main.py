37e27122c910df19dc5e9bd18aa1b5551b675a22
Galla Yagnesh <gallayagnesh@gmail.com>
2025-03-29 15:57:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -137,7 +137,7 @@ def index():
         return "GCS_BUCKET_NAME is not set", 500
 
     images = list_uploaded_images(bucket_name)
-    bg_color = os.getenv("BACKGROUND_COLOR", "#f0f2f5")  # Default to original color if not set
+    bg_color = os.getenv("BACKGROUND_COLOR", "#45b6fe")  # Default to original color if not set
     return render_template('index.html', images=images, bg_color=bg_color)
 
 @app.route('/upload', methods=['POST'])
